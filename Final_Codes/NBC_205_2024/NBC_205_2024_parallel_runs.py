f4547b0347474ab5fdefdd380c4fe3a38f16c034
Niraj Kumar Yadav <078bce094.niraj@pcampus.edu.np>
2026-05-15 14:16:22 +0545

    Update NBC_205_2024_parallel_runs.py

--- Final_Codes/NBC_205_2024/NBC_205_2024_parallel_runs.py
+++ Final_Codes/NBC_205_2024/NBC_205_2024_parallel_runs.py
@@ -1138,7 +1138,7 @@ def run_analysis(u, CarlSagan):
     ops.system('BandGen')
     ops.test('NormDispIncr', 1e-5, 20)
     ops.algorithm('Newton')
-    ops.integrator('LoadControl', 0.001)
+    ops.integrator('LoadControl', 1.0)
     ops.analysis('Static')
 
     ops.analyze(1)
